5e45f35f68be24cce748f6ff7641161093f4fe0d
Yusuf <mohdyusufhesam@gmail.com>
2023-07-30 21:51:27 +0530

    Termination hotkey

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ import urllib.request
 import winsound
 import time
 import pyautogui
+import win32api
+
+def hot_key_press():
+     if win32api.GetKeyState(0x4D) < 0 and win32api.GetKeyState(0x4B) < 0 and win32api.GetKeyState(0x43)<0:
+          return True
 
 def check_connection(host='https://www.google.com'):
     try:
@@ -28,3 +33,5 @@ while t:
             for i in range(500):
                 winsound.Beep(750, 800)
                 time.sleep(0.3)
+                if hot_key_press():
+                    break
